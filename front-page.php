9479c30d1e4f791947384eb77c11c3997fea31eb
Duncia <robertas.duncia@gmail.com>
2025-02-26 22:33:31 +0200

    Initial2

--- front-page.php
+++ front-page.php
@@ -8,7 +8,7 @@ HOME PAGE template
 <?php get_header(); ?>
 
 <section class="container">
-    <h4>Cia HOME page</h4>
+    <h4 class="pt-md pb-md">Cia HOME page</h4>
 </section>
 
 <?php get_footer(); ?>
